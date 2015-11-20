bdf51c5ce225e9b888bc62ccb10df43e5fad135a
Adrien Bigler <adrienbigler@Adriens-MacBook-Pro.local>
2015-11-20 21:47:17 +0100

    4eme and 3eme AFF scorers are ready

---
+++ js/4eme-ligue.js
@@ -0,0 +1,74 @@
+var region;
+$(document).ready(function(){
+    if(document.getElementById('aff-4') !== null){
+        region = 'aff'
+        var minGoals = 1;
+        var scorerRankingGroup1 = 1;
+        var scorerRankingGroup2 = 1;
+        var scorerRankingGroup3 = 1;
+        var scorerRankingGroup4 = 1;
+        var scorerRankingGroup5 = 1;
+        
+        $.getJSON( 'http://footballtopscorers-pmeweb.rhcloud.com/scorerRanking/4/aff',function(data){
+            $('.loader').hide()
+            $.each( data.scorers["1"], function( i, scorer ) {
+                if(scorer.goals > minGoals){
+                    $('#group1 tbody').append('<tr><td>'+scorerRankingGroup1+'</td><td>'+scorer.scorer+'</td><td>'+scorer.team+'</td><td>'+scorer.goals+'</td></tr>')
+                    scorerRankingGroup1++
+                }
+            });
+            $.each( data.scorers["2"], function( i, scorer ) {
+                if(scorer.goals > minGoals){
+                    $('#group2 tbody').append('<tr><td>'+scorerRankingGroup2+'</td><td>'+scorer.scorer+'</td><td>'+scorer.team+'</td><td>'+scorer.goals+'</td></tr>')
+                    scorerRankingGroup2++
+                }
+            });
+            $.each( data.scorers["3"], function( i, scorer ) {
+                if(scorer.goals > minGoals){
+                    $('#group3 tbody').append('<tr><td>'+scorerRankingGroup3+'</td><td>'+scorer.scorer+'</td><td>'+scorer.team+'</td><td>'+scorer.goals+'</td></tr>')
+                    scorerRankingGroup3++
+                }
+            });
+            $.each( data.scorers["4"], function( i, scorer ) {
+                if(scorer.goals > minGoals){
+                    $('#group4 tbody').append('<tr><td>'+scorerRankingGroup4+'</td><td>'+scorer.scorer+'</td><td>'+scorer.team+'</td><td>'+scorer.goals+'</td></tr>')
+                    scorerRankingGroup4++
+                }
+            });
+            $.each( data.scorers["5"], function( i, scorer ) {
+                if(scorer.goals > minGoals){
+                    $('#group5 tbody').append('<tr><td>'+scorerRankingGroup5+'</td><td>'+scorer.scorer+'</td><td>'+scorer.team+'</td><td>'+scorer.goals+'</td></tr>')
+                    scorerRankingGroup5++
+                }
+            });
+            $('.lastUpdate').append(data.lastUpdate)
+        })
+    }
+
+})
+
+var rankingChoice = {
+    groupCount: 0,
+    init: function(nbGroups){
+        var group, rankingType;
+        rankingChoice.groupCount = nbGroups;
+        rankingChoice.hideRankings()
+        $('#global, #global-standard').show()
+        rankingChoice.groupChoice()
+    },
+    groupChoice: function(){
+        $("#group-choice, #rank-choice").on('change',function() {
+            rankingChoice.hideRankings();
+            group = $("#group-choice option:selected").val()
+            rankingType = $("#rank-choice option:selected").val()
+            $('#'+group).show()
+            $('#'+group+'-'+rankingType).show() 
+        });
+    },
+    hideRankings: function(){
+        for (var i = 1; i <= rankingChoice.groupCount; i++) {
+            $('#group'+i).hide()
+        };
+        $('#global, .ranking').hide()
+    }
+}
