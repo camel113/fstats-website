26fe6093dcdc46d622deb856001d257835371a06
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-06-30 10:46:56 +0200

    DRY group defence ranking

--- js/dev/3eme-ligue-1.2.2.js
+++ js/dev/3eme-ligue-1.2.2.js
@@ -51,12 +51,6 @@ $(document).ready(function(){
     }
     
     if(document.getElementById('aff-3-teams') !== null){
-        var attackRankingGroup1 = 1;
-        var defenseRankingGroup1 = 1;
-        var attackRankingGroup2 = 1;
-        var defenseRankingGroup2 = 1;
-        var attackRankingGroup3 = 1;
-        var defenseRankingGroup3 = 1;
         var globalRanking = 1;
         var globalRankingDefense = 1;
         var globalRankingAttack = 1;
@@ -114,39 +108,17 @@ $(document).ready(function(){
             })
         })
 
-        var teamAttacksRankingManager = {
-            attackRankingGroup1:0,
-            attackRankingGroup2:0,
-            attackRankingGroup3:0,
-            add: function(groupId,team,groupNumber){
-                var ranking = 0
-                if(groupNumber == 1){
-                    ranking = attackRankingGroup1++
-                }
-                if(groupNumber == 2){
-                    ranking = attackRankingGroup2++
-                }
-                if(groupNumber == 3){
-                    ranking = attackRankingGroup3++
-                }
-                $(groupId).find('tbody').append('<tr><td>'+ranking+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td><strong>'+team.goalsfor+'</strong></td><td>'+team.goalsagainst+'</td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
-            }
-        }
-
 
         $.getJSON('http://footballtopscorers-pmeweb.rhcloud.com/teams/defense/aff/3',function(data){
             $.each( data.teams,function(i, team){
                 if(team.group == 1){
-                    $('#group1-defense tbody').append('<tr><td>'+defenseRankingGroup1+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td>'+team.goalsfor+'</td><td><strong>'+team.goalsagainst+'</strong></td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
-                    defenseRankingGroup1++;
+                    teamDefencesRankingManager.add("#group1-defense",team,1)
                 }
                 if(team.group == 2){
-                    $('#group2-defense tbody').append('<tr><td>'+defenseRankingGroup2+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td>'+team.goalsfor+'</td><td><strong>'+team.goalsagainst+'</strong></td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
-                    defenseRankingGroup2++;
+                    teamDefencesRankingManager.add("#group2-defense",team,2)
                 }
                 if(team.group == 3){
-                    $('#group3-defense tbody').append('<tr><td>'+defenseRankingGroup3+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td>'+team.goalsfor+'</td><td><strong>'+team.goalsagainst+'</strong></td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
-                    defenseRankingGroup3++;
+                    teamDefencesRankingManager.add("#group3-defense",team,3)
                 }
                 $('#global-defense tbody').append('<tr><td>'+globalRankingDefense+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td>'+team.group+'</td><td>'+team.goalsfor+'</td><td><strong>'+team.goalsagainst+'</strong></td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
                 globalRankingDefense++;
@@ -333,6 +305,45 @@ var teamRankingManager = {
         $(groupId).find('tbody').append('<tr><td>'+team.rank+'</td><td><a class="chart-link" href="#" data-group="'+dataGroup+'">'+team.team+'</a></td><td>'+team.goalsfor+'</td><td>'+team.goalsagainst+'</td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td><strong>'+team.points+'</strong></td></tr>')
     }
 }
+
+var teamAttacksRankingManager = {
+    attackRankingGroup1:1,
+    attackRankingGroup2:1,
+    attackRankingGroup3:1,
+    add: function(groupId,team,groupNumber){
+        var ranking = 0
+        if(groupNumber == 1){
+            ranking = teamAttacksRankingManager.attackRankingGroup1++
+        }
+        if(groupNumber == 2){
+            ranking = teamAttacksRankingManager.attackRankingGroup2++
+        }
+        if(groupNumber == 3){
+            ranking = teamAttacksRankingManager.attackRankingGroup3++
+        }
+        $(groupId).find('tbody').append('<tr><td>'+ranking+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td><strong>'+team.goalsfor+'</strong></td><td>'+team.goalsagainst+'</td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
+    }
+}
+
+var teamDefencesRankingManager = {
+    defenseRankingGroup1:1,
+    defenseRankingGroup2:1,
+    defenseRankingGroup3:1,
+    add: function(groupId,team,groupNumber){
+        var ranking = 0
+        if(groupNumber == 1){
+            ranking = teamDefencesRankingManager.defenseRankingGroup1++
+        }
+        if(groupNumber == 2){
+            ranking = teamDefencesRankingManager.defenseRankingGroup2++
+        }
+        if(groupNumber == 3){
+            ranking = teamDefencesRankingManager.defenseRankingGroup3++
+        }
+        $(groupId).find('tbody').append('<tr><td>'+ranking+'</td><td>'+team.team+'</td><td>'+team.rank+'</td><td>'+team.goalsfor+'</td><td><strong>'+team.goalsagainst+'</strong></td><td>'+team.won+'</td><td>'+team.tied+'</td><td>'+team.lost+'</td><td>('+team.fairplay+')</td><td>'+team.points+'</td></tr>')
+    }
+}
+
 
 var makeChart = {
     init: function(dataset){
