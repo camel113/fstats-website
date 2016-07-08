68bf40d7c8ac865e68b631405e59581db71c4269
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-07-08 16:22:50 +0200

    Automate Team standard graph

--- js/dev/3eme-ligue.js
+++ js/dev/3eme-ligue.js
@@ -1,16 +1,25 @@
 var lineColors = ['#F44336','#9C27B0','#3F51B5','#2196F3','#009688','#8BC34A','#FFEB3B','#FF9800','#795548','#E91E63','#673AB7','#00BCD4','#CDDC39']
 $(document).ready(function(){
 
-	var group1DataSet = [];
+    var groups = []
 
-	$('#group1-standard .table tbody tr').each(function(){
-		var teamName = ($(this).find("td:nth-child(2)").text())
-		var evolution = ($(this).data('evolution'))
-		group1DataSet.push({label:teamName,data:evolution,fill:false,borderColor:lineColors[group1DataSet.length],pointBackgroundColor:lineColors[group1DataSet.length],backgroundColor:lineColors[group1DataSet.length]})
-	})
-	$('#group1-standard .table tbody tr a').on('click',function(){
-		makeChart.init(group1DataSet)
-	})
+    var group1DataSet = []
+    $('.team-standard').each(function(){
+        var groupId = $(this).attr('id')
+        groups[groupId] = []
+        $(this).find('table tbody tr').each(function(){
+            var teamName = ($(this).find("td:nth-child(2)").text())
+            var evolution = ($(this).data('evolution'))
+            groups[groupId].push({label:teamName,data:evolution,fill:false,borderColor:lineColors[groups[groupId].length],pointBackgroundColor:lineColors[groups[groupId].length],backgroundColor:lineColors[groups[groupId].length]})
+        })
+        $(this).find('.table tbody tr a').each(function(){
+            $(this).on('click',function(e){
+                event.preventDefault();
+                console.log($(this))
+                makeChart.init(groups[groupId])
+            })
+        })
+    })
 })
 
 var makeChart = {
