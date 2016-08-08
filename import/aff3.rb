ca970204bb2b86fc17b2ca5015640a46e0177f4e
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-08-08 08:46:32 +0200

    Fix bug for data import

--- import/aff3.rb
+++ import/aff3.rb
@@ -76,7 +76,6 @@ def create_directories(filePath)
 end
 
 def generate_stats_for_regions_league(league,region,filePath,urlPath)
-  File.delete(filePath+"stats/scorersByLeagueAndRegion.json")
   response = HTTParty.get(urlPath+'/stats/top10scorers/'+region+'/'+league.to_s)
   File.open(filePath+"stats/scorers.json","w") do |f|
     f.write(response.body)
