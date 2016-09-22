3d249fa50e35ccb81e37f9ce3cd5c2bf53ae93f3
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-09-22 23:01:36 +0200

    Add evolution to team rankings

--- js/dev/abg.js
+++ js/dev/abg.js
@@ -78,7 +78,7 @@ define(["jquery","bootstrap","jquery_magnific_popup","jquery_easing","jquery_fle
     var makeChart = {
         init: function(dataset){
             console.log("###")
-            var myLabels = ["08.09","15.09"]
+            var myLabels = ["08.09","15.09","22.09"]
             var labelsToRemoveToFitData = (myLabels.length - dataset[0].data.length)
             if( labelsToRemoveToFitData > 0){
                 for (i = 0; i < labelsToRemoveToFitData; i++) { 
