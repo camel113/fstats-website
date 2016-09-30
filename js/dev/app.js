076ea7be30ec520d601fed813937977f9a3ad106
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-09-30 12:01:18 +0200

    Evolution

--- js/dev/app.js
+++ js/dev/app.js
@@ -5,7 +5,7 @@
 // directory.
 requirejs.config({
     baseUrl: '/js',
-    urlArgs: "bust=v1.0.1",
+    urlArgs: "bust=v1.0.2",
     paths: {
         app: './',
         jquery_magnific_popup: "jquery.magnific-popup.min",
