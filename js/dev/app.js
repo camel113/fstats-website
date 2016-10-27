aed2f5a81bd88126782ed33cc37e240becbd65bf
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-10-27 11:14:44 +0200

    New evolution step

--- js/dev/app.js
+++ js/dev/app.js
@@ -5,7 +5,7 @@
 // directory.
 requirejs.config({
     baseUrl: '/js',
-    urlArgs: "bust=v1.0.6",
+    urlArgs: "bust=v1.0.7",
     paths: {
         app: './',
         jquery_magnific_popup: "jquery.magnific-popup.min",
