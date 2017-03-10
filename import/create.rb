a5663fec1fe23bc96c5e50b4ce93350135f857a4
camel113 <adrienbigler@gmail.com>
2017-03-10 18:08:13 +0100

    Post are generated automatically with their images

--- import/create.rb
+++ import/create.rb
@@ -3,72 +3,11 @@ require 'httparty'
 require 'fileutils'
 require 'json'
 
-File.open("posts-images/image-mobile.html","w") do |f|
-	
-	f.write('---')
-	f.write "\n"
-	f.write "layout: resume-region-image"
-	f.write "\n"
-	f.write('---')
-	f.write "\n"
-	# puts jsondata['top5scorers']
-	f.write '<div class="stats-social-container mobile aff" id="resume">'
-	f.write('<div class="stats-table-social-post"><table class="table">')
-	f.write('<thead><tr><th><i class="fa fa-male"></i></th><th>Ligue</th><th><i class="fa fa-futbol-o"></i></th></tr></thead>')
-	f.write('<tbody>')
-	file = File.read('_data/acgf/ligue2/stats/scorers.json')
-	jsondata = JSON.parse(file)
-	previousGoals = 0
-	jsondata['top5scorers'].each_with_index do |i,index|
-   	if index >= 1 && i["goals"] != previousGoals
-   		f.write("</td>")
-   		f.write("<td>3</td>")
- 			f.write("<td>#{i['goals']}</td>")
-   		f.write('</tr>')
-   		break
-   	else
-   		puts "#{i['scorer']} #{i['goals']}"
-   		puts "#{index}"
-   		if index == 0
-   			f.write('<tr>')
-   			f.write("<td>")
- 			end
-   		f.write("<b>#{i['scorer']}</b>")
-   		f.write("<i> #{i['team']}</i></br>")
-   		previousGoals = i["goals"]
-   	end
-	end
-	file = File.read('_data/acgf/ligue3/stats/scorers.json')
-	jsondata = JSON.parse(file)
-	previousGoals = 0
-	jsondata['top5scorers'].each_with_index do |i,index|
-   	if index >= 1 && i["goals"] != previousGoals
-   		f.write("</td>")
-   		f.write("<td>3</td>")
- 			f.write("<td>#{i['goals']}</td>")
-   		f.write('</tr>')
-   		break
-   	else
-   		puts "#{i['scorer']} #{i['goals']}"
-   		puts "#{index}"
-   		if index == 0
-   			f.write('<tr>')
-   			f.write("<td>")
- 			end
-   		f.write("<b>#{i['scorer']}</b>")
-   		f.write("<i> #{i['team']}</i></br>")
-   		previousGoals = i["goals"]
-   	end
-	end
-	f.write('</tbody></table></div>')
-	f.write '</div>'
- 	
-end
 
 
 def create_post_image_by_region(region)
-
-	File.open("posts-images/image-"+region+".html","w") do |f|
+	date = Time.now.strftime("%Y-%m-%d")
+	File.open("posts-images/"+date+"-image-"+region+".html","w") do |f|
 	
 		f.write('---')
 		f.write "\n"
@@ -76,8 +15,48 @@ def create_post_image_by_region(region)
 		f.write "\n"
 		f.write('---')
 		f.write "\n"
-		# puts jsondata['top5scorers']
-		f.write('<div class="stats-social-container '+region+' id="resume">')
+		f.write('<div class="stats-social-container '+region+'" id="resume">')
+		f.write('<div class="stats-table-social-post"><table class="table">')
+		f.write('<thead><tr><th><i class="fa fa-male"></i></th><th>Ligue</th><th><i class="fa fa-futbol-o"></i></th></tr></thead>')
+		f.write('<tbody>')
+		[*2..5].each { |x| 
+			file = File.read('_data/'+region+'/ligue'+x.to_s+'/stats/scorers.json')
+			jsondata = JSON.parse(file)
+			previousGoals = 0
+			jsondata['top5scorers'].each_with_index do |i,index|
+		   	if index >= 1 && i["goals"] != previousGoals
+		   		f.write("</td>")
+		   		f.write("<td>"+x.to_s+"</td>")
+		 			f.write("<td>#{i['goals']}</td>")
+		   		f.write('</tr>')
+		   		break
+		   	else
+		   		puts "#{i['scorer']} #{i['goals']}"
+		   		puts "#{index}"
+		   		if index == 0
+		   			f.write('<tr>')
+		   			f.write("<td>")
+		 			end
+		   		f.write("<b>#{i['scorer']}</b>")
+		   		f.write("<i> #{i['team']}</i></br>")
+		   		previousGoals = i["goals"]
+		   	end
+			end
+		}
+		f.write('</tbody></table></div>')
+		f.write '</div>'
+	 	
+	end
+
+	File.open("posts-images/"+date+"-image-mobile-"+region+".html","w") do |f|
+	
+		f.write('---')
+		f.write "\n"
+		f.write "layout: resume-region-image"
+		f.write "\n"
+		f.write('---')
+		f.write "\n"
+		f.write('<div class="stats-social-container '+region+' mobile" id="resume">')
 		f.write('<div class="stats-table-social-post"><table class="table">')
 		f.write('<thead><tr><th><i class="fa fa-male"></i></th><th>Ligue</th><th><i class="fa fa-futbol-o"></i></th></tr></thead>')
 		f.write('<tbody>')
@@ -113,18 +92,29 @@ end
 
 
 def create_post_by_region(region)
-	File.open("_posts/2017-03-11-resume-"+region+".md","w") do |f|
+	date = Time.now.strftime("%Y-%m-%d")
+	File.open("_posts/"+date+"-resume-"+region+".md","w") do |f|
 		f.write('---')
 		f.write "\n"
 		f.write "layout: post"
 		f.write "\n"
 		f.write "title: "+region.upcase+" - Les stats de la semaine"
 		f.write "\n"
-		f.write('date:   2017-03-11 17:15:00 +0200')
+		f.write('date: '+date+' 11:00:00 +0200')
+		f.write "\n"
+		f.write('image: '+date+'-image-'+region+'.png')
+		f.write "\n"
+		f.write('image-mobile: '+date+'-image-'+region+'-mobile.png')
+		f.write "\n"
+		f.write('categories: resume '+region)
 		f.write "\n"
 		f.write('---')
 		f.write "\n"
-		f.write "<p>Chaque semaine retrouvez les meilleurs buteurs "+region.upcase+" de la 2e à la 5e ligue.</p>"
+		f.write "<p>Retrouvez les meilleurs buteurs "+region.upcase+" des championnats de football amateur de la 2e à la 5e ligue.</p>"
+		f.write "\n"
+		f.write "\n"
+		f.write "<p>Si vous constatez des erreurs ou des oublis dans nos classements. Laissez-nous un message sur Facebook ou Instagram. Vous avez aussi la possiblité de nous écrire à contact@footstats.ch ou par whatsapp au +41774502373.</p>"
+		f.write "\n"
 		[*2..5].each { |x| 
 			f.write "<p>Les meilleurs buteurs de #{x}ème ligue</p>"
 			f.write('<table class="table">')
@@ -158,3 +148,4 @@ regions.each { |r|
   create_post_by_region(r)
   create_post_image_by_region(r)
 }
+result = exec("phantomjs import/screen.js")
--- import/screen.js
+++ import/screen.js
@@ -1,16 +1,49 @@
 var page = require('webpage').create();
-page.open('http://localhost:4000/posts-images/image.html', function() {
-	var height = page.evaluate(function(){
-    return document.getElementById('resume').offsetHeight;
-  }); 
-  var width = page.evaluate(function(){
-    return document.getElementById('resume').offsetWidth;
-  }); 
-  console.log('Crop to: '+width+"x"+height);
+var async = require('async');
+var moment = require('moment');
 
-  page.clipRect = { top: 0, left: 0, width: width, height: height };
-  window.setTimeout(function () {
-    page.render('_image.png');
-    phantom.exit();
-  }, 200);
+var today = (moment().format('YYYY-MM-DD'))
+
+var regions = ["aff","acgf","avf","anf","acvf"]
+async.eachSeries(regions, function(item, callback) {
+	page.open('http://localhost:4000/posts-images/'+today+'-image-'+item+'.html', function() {
+		console.log('http://localhost:4000/posts-images/'+today+'-image-'+item+'.html')
+		var height = page.evaluate(function(){
+	    return document.getElementById('resume').offsetHeight;
+	  }); 
+	  var width = page.evaluate(function(){
+	    return document.getElementById('resume').offsetWidth;
+	  }); 
+	  console.log(item)
+	  console.log('Crop to: '+width+"x"+height);
+
+	  page.clipRect = { top: 0, left: 0, width: width, height: height };
+	  window.setTimeout(function () {
+	    page.render('images/posts/headers/'+today+'-image-'+item+'.png');
+	    callback()
+	  }, 500);
+	})
+},function(){
+	async.eachSeries(regions, function(item, callback) {
+		page.open('http://localhost:4000/posts-images/'+today+'-image-mobile-'+item+'.html', function() {
+			console.log('http://localhost:4000/posts-images/'+today+'-image-mobile-'+item+'.html')
+			var height = page.evaluate(function(){
+		    return document.getElementById('resume').offsetHeight;
+		  }); 
+		  var width = page.evaluate(function(){
+		    return document.getElementById('resume').offsetWidth;
+		  }); 
+		  console.log(item)
+		  console.log('Crop to: '+width+"x"+height);
+
+		  page.clipRect = { top: 0, left: 0, width: width, height: height };
+		  window.setTimeout(function () {
+		    page.render('images/posts/headers/'+today+'-image-'+item+'-mobile.png');
+		    callback()
+		  }, 500);
+		})
+	},function(){
+		phantom.exit();
+		console.log("phatomjs finish")
+	})
 });
