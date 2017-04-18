3e0d29feaf56bfe2493e1e52ef6e499a1a516a5d
camel113 <adrienbigler@gmail.com>
2017-04-18 14:24:11 +0200

    Gulp config
    - Styles
    - Jekyll (partially)

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,28 @@ var reload = browserSync.reload;
 var cp = require('child_process');
 var gutil = require('gulp-util');
 var run = require('gulp-run');
+var sass = require('gulp-sass');
 
-// Set the path variables
-const base_path = './',
-  src = base_path + '_dev/src',
-  dist = base_path + 'assets',
-  paths = {  
-    js: src + '/js/*.js',
-    scss: [ src +'/sass/*.scss',
-            src +'/sass/**/* .scss',
-            src +'/sass/**/**/*.scss'],
-    jekyll: ['index.html', '_posts/*', '_layouts/*', '_includes/*']
-  };
+const base_path = './';
+var assetsDevDir = {
+  path: base_path + '_assets/',
+}
+assetsDevDir.styles = assetsDevDir.path + 'styles/*.scss';
+var siteDir = {
+  path: base_path + '_site/',
+  assets: {}
+};
+siteDir.assets.path = siteDir.path + 'assets/';
+siteDir.assets.styles = siteDir.assets.path + 'styles/';
+siteDir.assets.js = siteDir.assets.path + 'js/'
+var jekyllDir = {
+  assets: {
+    path: base_path + 'assets/',
+  }
+};
+jekyllDir.assets.styles = jekyllDir.assets.path + 'styles/';
+jekyllDir.assets.js = jekyllDir.assets.path + 'js/';
+jekyllDir.jekyll = ['./*.html', '_posts/*', '_layouts/*', '_includes/*']
 
 // watch files for changes and reload
 gulp.task('serve', function() {
@@ -25,17 +35,28 @@ gulp.task('serve', function() {
     },
     port: 4000
   });
-  gulp.watch(paths.jekyll, ['build:jekyll:watch']);
+  gulp.watch(jekyllDir.jekyll, ['build:jekyll:watch']);
+  gulp.watch(assetsDevDir.styles, ['sass:dev']);
 });
 
-gulp.task('build:jekyll:local', function() {
+gulp.task('build:jekyll:dev', function() {
     var shellCommand = 'bundle exec jekyll build --config _config.yml';
-
     return gulp.src('')
       .pipe(run(shellCommand))
       .on('error', gutil.log);
 });
-gulp.task('build:jekyll:watch', ['build:jekyll:local'], function(callback) {
+gulp.task('build:jekyll:watch', ['build:jekyll:dev'], function(callback) {
     browserSync.reload();
     callback();
+});
+gulp.task('sass:dev', function () {
+  return gulp.src(assetsDevDir.styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(siteDir.assets.styles))
+    .pipe(reload({stream: true}));
+});
+gulp.task('sass:prod', function () {
+  return gulp.src(assetsDevDir.styles)
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest(jekyllDir.assets.styles))
 });
