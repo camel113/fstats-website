6db8fd0fcfa2fe31395cfb446f3856b267923a83
camel113 <adrienbigler@gmail.com>
2017-04-20 13:56:33 +0200

    Infinite scroll

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,8 +2,48 @@
   $(function() {
     navManager.init()
     rankingChoice.init()
+    infiniteScroll.init()
   });
 })(jQuery);
+
+var infiniteScroll = {
+  //https://blog.codestack.de/2015/05/17/seo-friendly-infinite-scroll.html
+  init: function(){
+    $(window).scroll(function() {
+      // get current scroll top in px, add window height and
+      // check if this is greater than the document height minus 300
+      if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
+        infiniteScroll.loadMoreContent();
+      }
+    })
+  },
+  loadMoreContent: function(){
+    console.log("LOADMORE")
+    var next = $("a.next").first();
+    next.each(function(key, value) {
+      var url = $(value).attr('href');
+      // remove button, so we can't load multiple times
+      next.remove();
+      // adding a temp. dom object to load the next page
+      var list = $("<div></div>");
+      // call load method and get the div.post-list node
+      list.load(url + " div.container", function(response, status, xhr) {
+        // check if we doesn't get any error
+        if ( status != "error" ) {
+          // copy all childrens of our temp container to the real container
+          // note: jQuery load will copy the div.post-list node as well,
+          // so use the childrens and move them
+          var container = $("div.container");
+          list.children("div.container").children().each(
+            function(key, value){
+              container.append(value);
+            }
+          );
+        }
+      });
+    });
+  }
+}
 
 var navManager = {
   init: function(){
