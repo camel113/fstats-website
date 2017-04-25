da12b0cf35508db95a6db01676fa2942a0f323da
camel113 <adrienbigler@gmail.com>
2017-04-25 09:31:52 +0200

    Switch between teams and scorers
    Need to check some stuffs
    - Desktop rankings style
    - Button style
    - Women

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,9 +1,42 @@
 (function($) {
   $(function() {
     navManager.init()
-    rankingChoice.init()
+    if($('.ligue-teams').length>0){
+      rankingChoice.init()
+    }
     infiniteScroll.init()
-    $('#league-stats').smoothState();
+    var options = {
+      prefetch: true,
+      cacheLength: 2,
+      onStart: {
+        duration: 250, // Duration of our animation
+        render: function ($container) {
+          // Add your CSS animation reversing class
+          $container.addClass('is-exiting');
+
+          // Restart your animation
+          smoothState.restartCSSAnimations();
+        }
+      },
+      onReady: {
+        duration: 0,
+        render: function ($container, $newContent) {
+          // Remove your CSS animation reversing class
+          $container.removeClass('is-exiting');
+
+          // Inject the new content
+          $container.html($newContent);
+
+        }
+      },
+      onAfter: function($container, $newContent) {
+        if($('.ligue-teams').length>0){
+          rankingChoice.init()
+        }
+      }
+    }
+    var smoothState = $('#league-stats').smoothState(options).data('smoothState');
+
   });
 })(jQuery);
 
