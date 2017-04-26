f7a7e0f807171559501d70ab953ced2452365c3b
camel113 <adrienbigler@gmail.com>
2017-04-26 17:19:29 +0200

    Add back to top button when scrolling

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,6 +5,8 @@
       rankingChoice.init()
     }
     infiniteScroll.init()
+    backToTop.init()
+
     var options = {
       prefetch: true,
       cacheLength: 2,
@@ -32,6 +34,7 @@
       onAfter: function($container, $newContent) {
         if($('.ligue-teams').length>0){
           rankingChoice.init()
+          backToTop.init()
         }
       }
     }
@@ -75,6 +78,35 @@ var infiniteScroll = {
           );
         }
       });
+    });
+  }
+}
+
+var backToTop = {
+  init: function(){
+    var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+      ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+      if( $(this).scrollTop() > offset_opacity ) { 
+        $back_to_top.addClass('cd-fade-out');
+      }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+      event.preventDefault();
+      $('body,html').animate({
+        scrollTop: 0 ,
+        }, scroll_top_duration
+      );
     });
   }
 }
