0092449324ca198d043ec2280633403bf944bdbd
camel113 <adrienbigler@gmail.com>
2017-07-13 16:46:54 +0200

    Setup data for AFV

--- import/all.rb
+++ import/all.rb
@@ -286,6 +286,15 @@ filePath = "_data/avf/ligue5/"
 create_directories(filePath)
 leagues.push({:groups => groups, :region => region, :league => league, :filePath => filePath})
 
+# AFV 2eme ligue
+groups = [*1..1]
+region = 'afv'
+league = 2
+filePath = "_data/afv/ligue2/"
+create_directories(filePath)
+leagues.push({:groups => groups, :region => region, :league => league, :filePath => filePath})
+regions.push(region)
+
 leagues.each { |l| 
   create_league_data(l[:region],l[:league],l[:groups],l[:filePath],urlPath)
   create_scorers_data(l[:league],l[:region],l[:filePath],urlPath)
