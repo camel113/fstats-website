752b3b10beeaad76dcaa54a5b1c2433e01df3a70
camel113 <adrienbigler@gmail.com>
2017-07-27 15:36:10 +0200

    Remove unused files

--- js/national-team-1.js
+++
@@ -1,1 +0,0 @@
-var stars=0;$(document).ready(function(){console.log("STARS"),$(".joueur").on("click",function(){$(this).hasClass("selected")?($(this).removeClass("selected"),stars--):stars<3&&($(this).addClass("selected"),stars++)})});
